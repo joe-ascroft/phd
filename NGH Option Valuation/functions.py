38d42642ba339cafc7e40ce51111b55da7e86dad
Joe Ascroft <joeascroftnz1@gmail.com>
2021-09-08 11:45:02 +1200

    8 Sept 2021

--- NGH Option Valuation/functions.py
+++ NGH Option Valuation/functions.py
@@ -1,7 +1,7 @@
 ## pi_tax => corporate rate, c_tax => carbon tax ##
 
-def profit_t(E,Pt,vc,fc,pi_tax,c_tax):
-    pi = ((E*(Pt-vc-c_tax))-fc)*(1-pi_tax)
+def profit_t(E,P,vc,fc,pi_tax,c_tax):
+    pi = ((E*(P-vc-c_tax))-fc)*(1-pi_tax)
     return pi
 
 ## T => days of extraction remaining, r => effective daily interest rate ##
@@ -9,3 +9,20 @@ def profit_t(E,Pt,vc,fc,pi_tax,c_tax):
 def value_simple(T,r,pi):
     v = pi*(math.exp(-r*T))*(1/r)
     return v
+
+
+## differencing defns ##
+
+a_i_central = (sigma**2)*(P**2)/((p_step*2)*p_step) - eta*(mean-P)/(p_step*2)
+b_i_central = (sigma**2)*(P**2)/((p_step*2)*p_step) - eta*(mean-P)/(p_step*2)
+
+## forward differencing ##
+
+a_i_forward = (sigma**2)*(P**2)/((p_step*2)*p_step)
+b_i_forward = (sigma**2)*(P**2)/((p_step*2)*p_step) - eta*(mean-P)/(p_step*2)
+
+## backward differencing ##
+
+a_i_backward = (sigma**2)*(P**2)/((p_step*2)*p_step) - eta*(mean-P)/(p_step*2)
+b_i_backward = (sigma**2)*(P**2)/((p_step*2)*p_step)
+
